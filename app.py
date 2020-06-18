d8944d9fe660adecf251d861eb68b19792a0a2ad
shamim22feb <shamim22feb@gmail.com>
2020-06-18 13:14:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 23:23:55 2020
+
+@author: SAM
+"""
+
+import nltk
+import pickle
+from flask import render_template,Flask,request,url_for
+
+model = pickle.load(open('model_nlp.pkl','rb'))
+cv = pickle.load(open('transform.pkl','rb'))
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+    render_template('home.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST' :
+        message = request.form['message']
+        data=[message]
+        vect = cv.transform(data).toarray()
+        predict = model.predict(vect)
+    return render_template('result.html',prediction = predict)
+    
